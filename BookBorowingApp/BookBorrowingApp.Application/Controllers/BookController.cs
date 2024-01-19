f064f77efd530b42c49609dba168a523bf798857
Rahul Dey <rahuldey832000@gmail.com>
2024-01-19 18:00:49 +0530

    Add: Book Controller

--- BookBorowingApp/BookBorrowingApp.Application/Controllers/BookController.cs
+++ BookBorowingApp/BookBorrowingApp.Application/Controllers/BookController.cs
@@ -1,34 +1,45 @@
 using System.Net;
 using BookBoowingApp.Domain.Common;
 using BookBoowingApp.Service.IServices;
+using BookBoowingApp.Service.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookBorrowingApp.Application.Controllers;
 
+/// <summary>
+/// Books api controller.
+/// </summary>
+/// <param name="bookService">The interface for book service.</param>
 [ApiController]
 [Route("api/books")]
 public class BookController(IBookService bookService) : ControllerBase
 {
+    /// <summary>
+    /// The book service interface.
+    /// </summary>
     private readonly IBookService _bookService = bookService;
 
     /// <summary>
     /// Get all available books for borrowing by other users.
     /// </summary>
-    /// <returns>Return List of Books.</returns>
+    /// <returns>Return List of Books or 404.</returns>
     /// <exception cref="ApiException">Api Exception</exception>
     [HttpGet]
     public async Task<IActionResult> GetAllAvailableBooks()
     {
         try
         {
+            // Fetching all available books details from service.
             var allBooks = await _bookService.GetAllAvailableBooks();
 
+            // Check for empty book list.
             if (allBooks.Data.Count == 0)
             {
                 return NotFound();
             }
 
+            // Return the book list data with 200 Status code.
             return Ok(allBooks.Data);
         }
         catch (ApiException)
@@ -41,12 +52,11 @@ public class BookController(IBookService bookService) : ControllerBase
         }
     }
 
-
     /// <summary>
-    /// Get single book details.
+    /// Get single book details route.
     /// </summary>
     /// <param name="id">The id of the book.</param>
-    /// <returns>Returns single book details.</returns>
+    /// <returns>Returns single book details or 404.</returns>
     /// <exception cref="ApiException">Api Exception</exception>
     [HttpGet]
     [Route("{id}")]
@@ -54,13 +64,16 @@ public class BookController(IBookService bookService) : ControllerBase
     {
         try
         {
+            // Fetching book details from service.
             var bookDetails = await _bookService.GetBookDetails(id);
 
+            // Check for not found response
             if (bookDetails.StatusCode == HttpStatusCode.NotFound)
             {
                 return NotFound();
             }
 
+            // Returns book details data with 200 status code.
             return Ok(bookDetails.Data);
         }
         catch (ApiException)
@@ -72,4 +85,134 @@ public class BookController(IBookService bookService) : ControllerBase
             throw new ApiException(HttpStatusCode.InternalServerError, ex);
         }
     }
+
+    /// <summary>
+    /// Create new book route.
+    /// </summary>
+    /// <param name="bookModel">The details of the new book.</param>
+    /// <returns>Return the 201 response with newly created book data or 400. </returns>
+    /// <exception cref="ApiException">Api Exception.</exception>
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> CreateNewBook([FromBody] AddBookModel bookModel)
+    {
+        try
+        {
+            // Check for model validation.
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            // Creating new book.
+            var bookDetails = await _bookService.AddNewBook(bookModel);
+
+            // Check for 201 response code.
+            if (bookDetails.StatusCode == HttpStatusCode.Created)
+            {
+                return StatusCode((int)bookDetails.StatusCode, bookDetails.Data);
+            }
+
+            // return the response code if anything happened wrong.
+            return StatusCode((int)bookDetails.StatusCode);
+        }
+        catch (ApiException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new ApiException(HttpStatusCode.InternalServerError, ex);
+        }
+    }
+
+    /// <summary>
+    /// Update single book route.
+    /// </summary>
+    /// <param name="id">The id of the requested book.</param>
+    /// <param name="bookModel">The updated book details.</param>
+    /// <returns>Returns the updated book details or 404 or 400.</returns>
+    /// <exception cref="ApiException">The Api Exception.</exception>
+    [HttpPut]
+    [Route("{id}")]
+    [Authorize]
+    public async Task<IActionResult> UpdateBookDetails(Guid id, [FromBody] AddBookModel bookModel)
+    {
+        try
+        {
+            // Check for model validations.
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            // Updated the book details.
+            var bookDetails = await _bookService.UpdateBookById(id, bookModel);
+
+            // Check for 404 response code.
+            if (bookDetails.StatusCode == HttpStatusCode.NotFound)
+            {
+                return NotFound();
+            }
+
+            // Check for 200 response code.
+            if (bookDetails.StatusCode == HttpStatusCode.OK)
+            {
+                return Ok(bookDetails.Data);
+            }
+
+            // return the response code if anything goes wrong.
+            return StatusCode((int)bookDetails.StatusCode);
+        }
+        catch (ApiException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new ApiException(HttpStatusCode.InternalServerError, ex);
+        }
+    }
+
+    /// <summary>
+    /// Delete book route.
+    /// </summary>
+    /// <param name="id">The book id.</param>
+    /// <returns>Returns susses or not found response/</returns>
+    /// <exception cref="ApiException">The Api Exception.</exception>
+    [HttpDelete]
+    [Route("{id}")]
+    [Authorize]
+    public async Task<IActionResult> DeleteBook(Guid id)
+    {
+        try
+        {
+            // Updated the book details.
+            var response = await _bookService.DeleteBookById(id);
+
+            // Check for 404 response code.
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return NotFound();
+            }
+
+            // Check for 200 response code.
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                return Ok();
+            }
+
+            // return the response code if anything goes wrong.
+            return StatusCode((int)response.StatusCode);
+        }
+        catch (ApiException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new ApiException(HttpStatusCode.InternalServerError, ex);
+        }
+    }
+
 }
---
+++ BookBorowingApp/BookBorrowingApp.Application/Migrations/20240119122826_AddLastUpdateOnBookDB.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookBorrowingApp.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastUpdateOnBookDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "82e6aef5-18b7-43ab-a405-5e05ed105ec7");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "95465c41-cbbe-4719-9ac5-9e19dd186c0b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "daab2038-2447-44b3-986b-1cde34ff494c");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Books",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "Books",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "56aa059f-3819-42ce-a4c5-b57f50b880d0", "3", "Test", "TEST" },
+                    { "d7416d42-9dff-4907-96ac-272337679783", "2", "User", "USER" },
+                    { "e5b1b0f2-743c-4b9e-9836-249651da4bda", "1", "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "56aa059f-3819-42ce-a4c5-b57f50b880d0");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d7416d42-9dff-4907-96ac-272337679783");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e5b1b0f2-743c-4b9e-9836-249651da4bda");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "Books");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "82e6aef5-18b7-43ab-a405-5e05ed105ec7", "1", "Admin", "ADMIN" },
+                    { "95465c41-cbbe-4719-9ac5-9e19dd186c0b", "3", "Test", "TEST" },
+                    { "daab2038-2447-44b3-986b-1cde34ff494c", "2", "User", "USER" }
+                });
+        }
+    }
+}
