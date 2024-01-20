edb2547157806d3ec04fa064b7125a14433d7858
Rahul Dey <rahuldey832000@gmail.com>
2024-01-20 19:40:10 +0530

    Add: Angular Material to project

--- BookBorrowingFrontEnd/src/app/app.component.ts
+++ BookBorrowingFrontEnd/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
