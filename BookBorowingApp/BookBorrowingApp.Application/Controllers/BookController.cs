41cccc3e1ecc16a4ff21acd4aea53e4dc2ef5ff2
Rahul Dey <rahuldey832000@gmail.com>
2024-01-31 11:23:21 +0530

    Update: book controller

--- BookBorowingApp/BookBorrowingApp.Application/Controllers/BookController.cs
+++ BookBorowingApp/BookBorrowingApp.Application/Controllers/BookController.cs
@@ -1,8 +1,11 @@
 using System.Net;
+using System.Net.Mime;
 using BookBoowingApp.Domain.Common;
 using BookBoowingApp.Service.IServices;
 using BookBoowingApp.Service.Models;
+using BookBorrowingApp.Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 
@@ -25,8 +28,15 @@ public class BookController(IBookService bookService) : ControllerBase
     /// Get all available books for borrowing by other users.
     /// </summary>
     /// <returns>Return List of Books or 404.</returns>
+    /// <response code="200">Returns the list of books.</response>
+    /// <response code="404">Not found error if no books found.</response>
+    /// <response code="500">Internal server error.</response>
     /// <exception cref="ApiException">Api Exception</exception>
     [HttpGet]
+    [ProducesResponseType(200, Type = typeof(List<Book>))]
+    [ProducesResponseType(404, Type = typeof(NotFound))]
+    [ProducesResponseType(500, Type = typeof(ErrorResponse))]
+    [Produces(MediaTypeNames.Application.Json)]
     public async Task<IActionResult> GetAllAvailableBooks()
     {
         try
@@ -58,9 +68,16 @@ public class BookController(IBookService bookService) : ControllerBase
     /// </summary>
     /// <param name="id">The id of the book.</param>
     /// <returns>Returns single book details or 404.</returns>
+    /// <response code="200">Returns the book details.</response>
+    /// <response code="404">Not found error if no book found of specific id.</response>
+    /// <response code="500">Internal server error.</response>
     /// <exception cref="ApiException">Api Exception</exception>
     [HttpGet]
     [Route("{id}")]
+    [ProducesResponseType(200, Type = typeof(Book))]
+    [ProducesResponseType(404, Type = typeof(NotFound))]
+    [ProducesResponseType(500, Type = typeof(ErrorResponse))]
+    [Produces(MediaTypeNames.Application.Json)]
     public async Task<IActionResult> BookDetails(Guid id)
     {
         try
@@ -92,9 +109,21 @@ public class BookController(IBookService bookService) : ControllerBase
     /// </summary>
     /// <param name="bookModel">The details of the new book.</param>
     /// <returns>Return the 201 response with newly created book data or 400. </returns>
+    /// <response code="200">Returns the newly created book.</response>
+    /// <response code="400">Bad request with validation errors.</response>
+    /// <response code="500">Internal server error.</response>
+    /// <response code="401">Unauthorized.</response>
+    /// <response code="403">Forbidden.</response>
     /// <exception cref="ApiException">Api Exception.</exception>
     [HttpPost]
     [Authorize]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(201, Type = typeof(Book))]
+    [ProducesResponseType(400, Type = typeof(BadRequest))]
+    [ProducesResponseType(500, Type = typeof(ErrorResponse))]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [Produces(MediaTypeNames.Application.Json)]
     public async Task<IActionResult> CreateNewBook([FromBody] AddBookModel bookModel)
     {
         try
@@ -133,10 +162,24 @@ public class BookController(IBookService bookService) : ControllerBase
     /// <param name="id">The id of the requested book.</param>
     /// <param name="bookModel">The updated book details.</param>
     /// <returns>Returns the updated book details or 404 or 400.</returns>
+    /// <response code="200">Returns the updated book details.</response>
+    /// <response code="400">Bad request with validation errors.</response>
+    /// <response code="404">Not found error if no book found of specific id.</response>
+    /// <response code="500">Internal server error.</response>
+    /// <response code="401">Unauthorized.</response>
+    /// <response code="403">Forbidden.</response>
     /// <exception cref="ApiException">The Api Exception.</exception>
     [HttpPut]
     [Route("{id}")]
     [Authorize]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Book))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(BadRequest))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(NotFound))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ErrorResponse))]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [Produces(MediaTypeNames.Application.Json)]
     public async Task<IActionResult> UpdateBookDetails(Guid id, [FromBody] AddBookModel bookModel)
     {
         try
@@ -180,10 +223,21 @@ public class BookController(IBookService bookService) : ControllerBase
     /// </summary>
     /// <param name="id">The book id.</param>
     /// <returns>Returns susses or not found response/</returns>
+    /// <response code="200">Returns success response.</response>
+    /// <response code="404">Not found error if no book found with the specific id.</response>
+    /// <response code="500">Internal server error.</response>
+    /// <response code="401">Unauthorized.</response>
+    /// <response code="403">Forbidden.</response>
     /// <exception cref="ApiException">The Api Exception.</exception>
     [HttpDelete]
     [Route("{id}")]
     [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Ok))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(NotFound))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ErrorResponse))]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [Produces(MediaTypeNames.Application.Json)]
     public async Task<IActionResult> DeleteBook(Guid id)
     {
         try
@@ -216,9 +270,25 @@ public class BookController(IBookService bookService) : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Get all supported genre
+    /// </summary>
+    /// <returns>Returns List of string.</returns>
+    /// <response code="200">Returns list of supported genre.</response>
+    /// <response code="404">Not found error if no supported genre found.</response>
+    /// <response code="500">Internal server error.</response>
+    /// <response code="401">Unauthorized.</response>
+    /// <response code="403">Forbidden.</response>
+    /// <exception cref="ApiException">Api Exception</exception>
     [HttpGet]
     [Route("supported-genre")]
     [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<string>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(NotFound))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ErrorResponse))]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [Produces(MediaTypeNames.Application.Json)]
     public IActionResult GetSupportedGenres()
     {
         try
