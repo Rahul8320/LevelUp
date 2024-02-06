cc0ad1589c29b5595b8d895eb894d253f5c21c63
Rahul Dey <rahuldey832000@gmail.com>
2024-02-06 21:28:41 +0530

    Create book store page

--- BookBorrowingFrontEnd/src/app/app.routes.ts
+++ BookBorrowingFrontEnd/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './core/pages/login/login.component';
 import { RegisterComponent } from './core/pages/register/register.component';
 import { PageNotFoundComponent } from './core/pages/page-not-found/page-not-found.component';
 import { BookDetailsComponent } from './core/pages/book-details/book-details.component';
+import { BikestoreComponent } from './core/pages/bikestore/bikestore.component';
 
 export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent, title: "Dashboard Page" },
+    { path: 'bikestore', component: BikestoreComponent, title: "Bike Store Page" },
     { path: 'book-details/:bookId', component: BookDetailsComponent, title: "Book Details Page" },
     { path: 'login', component: LoginComponent, title: "Login Page" },
     { path: 'register', component: RegisterComponent, title: "Register Page" },
-    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: '', redirectTo: '/bikestore', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent, title: "Page Not Found" }
 ];
---
+++ BookBorrowingFrontEnd/src/app/core/pages/bikestore/bikestore.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bikestore',
+  standalone: true,
+  imports: [],
+  templateUrl: './bikestore.component.html',
+  styleUrl: './bikestore.component.css'
+})
+export class BikestoreComponent {
+
+}
