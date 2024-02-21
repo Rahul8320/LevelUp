6f4feff5490f2bdbbc5af75a6f7af69908064c2f
Rahul Dey <rahuldey832000@gmail.com>
2024-02-21 11:05:18 +0530

    Create Files

---
+++ LearnC#/CreateFiles/Program.cs
@@ -0,0 +1,10 @@
+﻿// Creating and Deleting Files.
+
+const string filename = "TestFile.txt";
+
+// Create new file - this will override any existing file of same name.
+// Use the "using" construct to automatically close the file stream.
+using (StreamWriter sw = File.CreateText(filename))
+{
+    sw.WriteLine("This file is automatically created by stream write using construct.");
+}
