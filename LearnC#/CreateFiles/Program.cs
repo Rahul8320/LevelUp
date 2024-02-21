8207a57e28a1118868fd5b1a289954a7414d37a1
Rahul Dey <rahuldey832000@gmail.com>
2024-02-21 16:05:28 +0530

    Read Write File

--- LearnC#/CreateFiles/Program.cs
+++ LearnC#/CreateFiles/Program.cs
@@ -10,15 +10,37 @@ const string filename = "TestFile.txt";
 // }
 
 // Check if the file is exist
-System.Console.WriteLine(File.Exists(filename));
-if (File.Exists(filename))
+// System.Console.WriteLine(File.Exists(filename));
+// if (File.Exists(filename))
+// {
+//     File.Delete(filename);
+//     System.Console.WriteLine($"File Deleted: {filename}");
+// }
+// else
+// {
+//     using StreamWriter sw = File.CreateText(filename);
+//     sw.WriteLine("This file is automatically created by stream write using construct.");
+//     System.Console.WriteLine($"File created: {filename}");
+// }
+
+// WriteAllText overwrites a file with the given content.
+Console.WriteLine($"Exist filename: {filename}: {File.Exists(filename)}");
+
+if (!File.Exists(filename))
 {
-    File.Delete(filename);
-    System.Console.WriteLine($"File Deleted: {filename}");
+    File.WriteAllText(filename, "This content is overwrite by write all text method.");
 }
-else
+
+// Append text into file
+File.AppendAllText(filename, "This text is appended by append all text method");
+
+using (StreamWriter sw = File.AppendText(filename))
 {
-    using StreamWriter sw = File.CreateText(filename);
-    sw.WriteLine("This file is automatically created by stream write using construct.");
-    System.Console.WriteLine($"File created: {filename}");
+    sw.WriteLine("Append one more line.");
+    sw.WriteLine("Another one by stream writer.");
+    sw.WriteLine("I promise this was the last one.");
 }
+
+// ReadAllText reads the file content
+string content = File.ReadAllText(filename);
+Console.WriteLine($"File Content: {content}");
