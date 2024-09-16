7d75d1f1dc13c754008873af8070b70562a69a9b
Rahul Dey <rahuldey832000@gmail.com>
2024-09-16 10:50:08 +0530

    Add basic requests in fast api

--- Bookly/main.py
+++ Bookly/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from pydantic import BaseModel 
 
 app = FastAPI()
 
@@ -9,3 +10,14 @@ async def read_root():
 @app.get("/greet/{name}")
 async def greet_user(name:str, age:int) -> dict:
     return {"message": f"Hello {name}", "age": age}
+
+class CreateBookModel(BaseModel):
+    title: str
+    author: str
+
+@app.post("/create_book", status_code = 201)
+async def create_book(request:CreateBookModel) -> dict:
+    return {
+        "title": request.title,
+        "author": request.author
+    }
